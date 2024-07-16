c246fec986bba7bc5308bd969aff702884e3f79b
mikic202 <mikolaj.chomanski@gmail.com>
2024-07-16 19:16:49 +0200

    Add basic plot

--- RobotDataWindow.py
+++ RobotDataWindow.py
@@ -1,7 +1,16 @@
 from UiRobotData import Ui_RobotDataWindow
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPixmap
 import matplotlib.pyplot as plt
+from io import BytesIO
+
+
+def generate_plot(x, y):
+    plt.plot(x, y)
+    buffor = BytesIO()
+    plt.savefig(buffor, format="png")
+    return buffor.getvalue()
 
 
 class RobotDataWindow(QMainWindow):
@@ -9,8 +18,13 @@ class RobotDataWindow(QMainWindow):
         super().__init__(parent)
         self.ui = Ui_RobotDataWindow()
         self.ui.setupUi(self)
+        self.draw_plot([1, 2, 3], [1, 2, 3])
 
-    def
+    def draw_plot(self, x, y):
+        image_data = generate_plot(x, y)
+        pixmap = QPixmap()
+        if pixmap.loadFromData(image_data):
+            self.ui.sensor_output.setPixmap(pixmap)
 
 
 if __name__ == "__main__":
