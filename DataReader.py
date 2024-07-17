af72b056bd0e7cb2e91c9d2afe86e79eb588c709
mikic202 <mikolaj.chomanski@gmail.com>
2024-07-17 20:01:50 +0200

    Add basic socket connection for sensors

--- DataReader.py
+++ DataReader.py
@@ -2,14 +2,27 @@ from typing import List
 from DataManipuation.SensorReading import SensorReading
 from DataManipuation.ControlValue import ControlValue
 from random import random
+import socket
+import json
+
+HOST = "localhost"
+SENSOR_DATA_PORT = 65432
 
 
 class DataReader:
     def __init__(self) -> None:
-        pass
+        self.sensor_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sensor_server.bind((HOST, SENSOR_DATA_PORT))
+        self.sensor_server.listen()
+        self.sensor_connection, _ = self.sensor_server.accept()
 
     def read_sensor_data(self) -> List[SensorReading]:
-        return [SensorReading(0, 0), SensorReading(1, random()), SensorReading(2, 2)]
+        request = self.sensor_connection.recv(1024)
+        request = request.decode("utf-8")
+        readings = []
+        for json_reading in json.loads(request):
+            readings.append(SensorReading(json_reading["angle"], json_reading["value"]))
+        return readings
 
     def read_control_values(self) -> List[ControlValue]:
         return [ControlValue("control", 0.5)]
